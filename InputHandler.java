a611e390847a7271691e960fcda5fdd778c59b50
DanielJones2021 <89530999+DanielJones2021@users.noreply.github.com>
2022-10-09 17:34:17 -0400

    Create InputHandler.java

---
+++ InputHandler.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class InputHandler {
+  private HashMap<String, Command> commands = new HashMap<String, Command>();
+  
+  public InputHandler(Document document) {
+    commands.put("view", new ViewCommand(document));
+    commands.put("save", new SaveCommand(docment));
+    commands.put("append", new AppendCommand(document));
+    commands.put("write", new WriteCommand(document));
+  }
+  
+  public void inputEntered(String data) {
+    if(commands.containsKey(Data))
+      commands.get(data).execute();
+    else
+        System.out.println("Command not known.");
+  }
+}
+    
