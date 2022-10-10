2bab0ab898ec883185d3f30742716348515df19e
LukeHatchl <lhatchl@email.sc.edu>
2022-10-09 22:31:09 -0400

    Edited methods in document class

--- command_DesignPattern/Document.java
+++ command_DesignPattern/Document.java
@@ -25,19 +25,23 @@ public class Document {
 
     public String append(String line){
         lines.add(line);
+        System.out.println("The line has been appended to the document");
         return "The line has been appended to the document";
     }
 
     public String write(String line){
         lines.removeAll(lines);
         lines.add(line);
+        System.out.println("The line was written to the file");
         return "The line was written to the file";
     }
     
     public String save(){
         if(FileManipulator.writeFile(filename, lines)) {
+            System.out.println("The file has been saved");
             return "The file has been saved";
         } else {
+            System.out.println("Sorry we cannot save at this time");
             return "Sorry we cannot save at this time";
         }
     }
